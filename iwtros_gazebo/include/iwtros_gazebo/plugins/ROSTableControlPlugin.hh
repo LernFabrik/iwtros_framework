f4494981c7cf413ac3b5175e6727ec1f0efe2ade
iwtros <prachandabhanu@gmail.com>
2019-02-15 10:47:43 +0100

    calcuate position from vel

--- iwtros_gazebo/include/iwtros_gazebo/plugins/ROSTableControlPlugin.hh
+++ iwtros_gazebo/include/iwtros_gazebo/plugins/ROSTableControlPlugin.hh
@@ -63,9 +63,11 @@ namespace gazebo{
         /*These offset are for the URDF reference
         howver it is not visible in the gazebo
         therefore this offset*/
-        private: std::float_t yawValue;
-        private: std::float_t lin_X;
-        private: std::float_t lin_Y;
+        private: std::float_t delta_theta, crnt_theta, old_theta;
+        private: std::float_t delta_x , crnt_x, old_x;
+        private: std::float_t delta_y, crnt_y, old_y;
+        private: std::float_t detlta_d, current_time, prev_time, dt;
+        private: std::int64_t loop_counter = 0;
         private: std::float_t off_yaw;
         private: std::string table_base_link;
 
