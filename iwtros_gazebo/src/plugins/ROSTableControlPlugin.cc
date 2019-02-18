073f3900aff599591ba2dffd1b1ceb01628095cc
iwtros <prachandabhanu@gmail.com>
2019-02-18 12:09:06 +0100

    TEST 2

--- iwtros_gazebo/src/plugins/ROSTableControlPlugin.cc
+++ iwtros_gazebo/src/plugins/ROSTableControlPlugin.cc
@@ -106,8 +106,8 @@ void ROSTableControlPlugin::MoveModel(float lin_x, float lin_y, float lin_z, flo
 
         this->detlta_d = sqrt((this->delta_x * this->delta_x) + (this->delta_y * this->delta_y));
         gzerr << "detla_d = " << this->detlta_d << "\n";
-        this->crnt_x = this->old_x + this->detlta_d * sin(this->old_theta + this->delta_theta / 2);
-        this->crnt_y = this->old_y + this->detlta_d * cos(this->old_theta + this->delta_theta / 2);
+        this->crnt_x = this->old_x + this->delta_x;//this->detlta_d * sin(this->old_theta + this->delta_theta / 2);
+        this->crnt_y = this->old_y + this->delta_y;//this->detlta_d * cos(this->old_theta + this->delta_theta / 2);
         this->crnt_theta = this->old_theta + this->delta_theta;
         gzerr << "calculated current pose of the model x = " << this->crnt_x << " y = " << this->crnt_y << " theta = " << this->crnt_theta << "\n";
     }else{
