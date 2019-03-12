de5da0f61b41ce85b39cfe00be3918858d610e45
iwtros <prachandabhanu@gmail.com>
2019-03-12 15:27:33 +0100

    async

--- iwtros_goal/src/ur5_async_task.cpp
+++ iwtros_goal/src/ur5_async_task.cpp
@@ -5,6 +5,11 @@
 #include <tf/tf.h>
 
 int main(int argc, char** argv){
+    ros::init(argc, argv, "ur5_async_motion");
     moveit::planning_interface::MoveGroupInterface move_group("ur5_arm");
-    move_group.setPoseReferenceFrame();
+    /*Refere http://docs.ros.org/jade/api/moveit_ros_planning_interface/html/classmoveit_1_1planning__interface_1_1MoveGroup.html#a92c9875f9f00a95693b3731d65a66ce1*/
+    std::vector<std::string> namedTargets = move_group.getNamedTargets(); // Get the nameds of the named robot states available as targets, both either remmbered state or default from SRDF
+    move_group.setNamedTarget("ur5_pick"); // Set the current ... or if not found, that are specified in the SRDF under the name "name" as a group stete.
+    move_group.asyncMove(); // Plan and execute a trajectory that takes the group of joint declared in the contructor to the specified traget. This call is always blocking(does not wait for the execution of the trjectory to complete)
+    
 }
