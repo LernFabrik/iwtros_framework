9f57409b4717bc5b9ac7856ca78ad89585c1c81f
iwtros <prachandabhanu@gmail.com>
2019-03-18 09:11:52 +0100

    iiwa hw move interface

--- iwtros_goal/scripts/iiwa_sb.py
+++ iwtros_goal/scripts/iiwa_sb.py
@@ -7,114 +7,97 @@
 ## To use the python interface to move_group, import the moveit_commander
 ## module.  We also import rospy and some messages that we will use.
 
-import sys
 import copy
-import rospy
-import moveit_commander
-import moveit_msgs.msg
+import sys
+
 import actionlib
 import geometry_msgs
+import moveit_commander
+import moveit_msgs.msg
+import rospy
+
+
+def getPosePoints(key):
+  choices = {1: (1, 1, 1), 
+            2: (2, 2, 2), 
+            3: (2, 2, 2), 
+            4: (2, 2, 2), 
+            5: (2, 2, 2), 
+            6: (2, 2, 2), 
+            7: (2, 2, 2)}
+  result = choices.get(key, ('default1', 'default2', 'default3', 'default4', 'default5', 'default6', 'default7'))
+  return result
+
 
 def simple_pick_place():
   ## First initialize moveit_commander and rospy.
   moveit_commander.roscpp_initialize(sys.argv)
   rospy.init_node('simple_movement',
                   anonymous=True)
-
   ## Instantiate a MoveGroupCommander object.  This object is an interface
   ## to one group of joints.  In this case the group refers to the joints of
-  ## robot1. This interface can be used to plan and execute motions on robot1.
-  iiwa_group = moveit_commander.MoveGroupCommander("robot1")
-  
+  ## IIWA7. This interface can be used to plan and execute motions on IIWA7.
+  iiwa_group = moveit_commander.MoveGroupCommander("manipulator")
   ## Action clients to the ExecuteTrajectory action server.
   iiwa_client = actionlib.SimpleActionClient('execute_trajectory',
                                             moveit_msgs.msg.ExecuteTrajectoryAction)
   iiwa_client.wait_for_server()
-  rospy.loginfo('Execute Trajectory server is available for robot1')
+  rospy.loginfo('Execute Trajectory server is available for IIWA7')
   ## Set a named joint configuration as the goal to plan for a move group.
   ## Named joint configurations are the robot poses defined via MoveIt! Setup Assistant.
-  robot1_group.set_named_target("R1Zero")
+  rate = rospy.Rate(20)
+  loopCounter = 1
+  while not rospy.is_shutdown():
+    #  ## Cartesian Paths
+    # ## ^^^^^^^^^^^^^^^
+    # ## You can plan a cartesian path directly by specifying a list of waypoints
+    # ## for the end-effector to go through.
+    waypoints = []
+    # start with the current pose
+    current_pose = iiwa_group.get_current_pose()
+    rospy.sleep(0.5)
+    current_pose = iiwa_group.get_current_pose()
+    
+    ## create linear offsets to the current pose
+    new_eef_pose = geometry_msgs.msg.Pose()
 
-  ## Plan to the desired joint-space goal using the default planner (RRTConnect).
-  plan = robot1_group.plan()
-  ## Create a goal message object for the action server.
-  robot1_goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
-  ## Update the trajectory in the goal message.
-  robot1_goal.trajectory = plan
+    # Get pose values
+    pose = getPosePoints(loopCounter)
+    new_eef_pose.position.x = pose[0]
+    new_eef_pose.position.y = pose[1]
+    new_eef_pose.position.z = pose[2]
+    # Retain orientation of the current pose.
+    new_eef_pose.orientation = copy.deepcopy(current_pose.pose.orientation)
+    waypoints.append(new_eef_pose)
+    waypoints.append(current_pose.pose)
+    ## We want the cartesian path to be interpolated at a resolution of 1 cm
+    ## which is why we will specify 0.01 as the eef_step in cartesian
+    ## translation.  We will specify the jump threshold as 0.0, effectively
+    ## disabling it.
+    fraction = 0.0
+    for count_cartesian_path in range(0,3):
+      if fraction < 1.0:
+        (plan_cartesian, fraction) = iiwa_group.compute_cartesian_path(
+                                    waypoints,   # waypoints to follow
+                                    0.01,        # eef_step
+                                    0.0)         # jump_threshold
+      else:
+        break
 
-  ## Send the goal to the action server.
-  robot1_client.send_goal(robot1_goal)
-  robot1_client.wait_for_result()
+    robot1_goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
+    robot1_goal.trajectory = plan_cartesian
 
-  robot1_group.set_named_target("R1PreGrasp")
-
-  plan = robot1_group.plan()
-  robot1_goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
-  robot1_goal.trajectory = plan
-
-  robot1_client.send_goal(robot1_goal)
-  robot1_client.wait_for_result()
-
-  #  ## Cartesian Paths
-  # ## ^^^^^^^^^^^^^^^
-  # ## You can plan a cartesian path directly by specifying a list of waypoints
-  # ## for the end-effector to go through.
-  waypoints = []
-  # start with the current pose
-  current_pose = robot1_group.get_current_pose()
-  rospy.sleep(0.5)
-  current_pose = robot1_group.get_current_pose()
-
-  ## create linear offsets to the current pose
-  new_eef_pose = geometry_msgs.msg.Pose()
-
-  # Manual offsets because we don't have a camera to detect objects yet.
-  new_eef_pose.position.x = current_pose.pose.position.x + 0.10
-  new_eef_pose.position.y = current_pose.pose.position.y - 0.20
-  new_eef_pose.position.z = current_pose.pose.position.z - 0.20
-
-  # Retain orientation of the current pose.
-  new_eef_pose.orientation = copy.deepcopy(current_pose.pose.orientation)
-
-  waypoints.append(new_eef_pose)
-  waypoints.append(current_pose.pose)
-
-  ## We want the cartesian path to be interpolated at a resolution of 1 cm
-  ## which is why we will specify 0.01 as the eef_step in cartesian
-  ## translation.  We will specify the jump threshold as 0.0, effectively
-  ## disabling it.
-  fraction = 0.0
-  for count_cartesian_path in range(0,3):
-    if fraction < 1.0:
-      (plan_cartesian, fraction) = robot1_group.compute_cartesian_path(
-                                   waypoints,   # waypoints to follow
-                                   0.01,        # eef_step
-                                   0.0)         # jump_threshold
-    else:
-      break
-
-  robot1_goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
-  robot1_goal.trajectory = plan_cartesian
-
-  robot1_client.send_goal(robot1_goal)
-  robot1_client.wait_for_result()
-
-
-  robot1_group.set_named_target("R1Place")
-
-  plan = robot1_group.plan()
-  robot1_goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
-  robot1_goal.trajectory = plan
-
-  robot1_client.send_goal(robot1_goal)
-  robot1_client.wait_for_result()
-
-  ## When finished shut down moveit_commander.
-  moveit_commander.roscpp_shutdown()
+    iiwa_client.send_goal(robot1_goal)
+    iiwa_client.wait_for_result()
+    loopCounter = loopCounter + 1
+    if loopCounter == 4:
+      loopCounter = 1
+    rate.sleep()
 
 
 if __name__=='__main__':
   try:
     simple_pick_place()
   except rospy.ROSInterruptException:
+    moveit_commander.roscpp_shutdown()
     pass
