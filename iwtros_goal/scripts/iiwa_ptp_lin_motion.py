3ab184102a1bf6f0215ca07190ba261063b089fa
Vader <prachachandabhanu@iwt-bodesee.de>
2019-07-10 16:44:39 +0200

    Panda HW is working but iiwa is not working.

--- iwtros_goal/scripts/iiwa_ptp_lin_motion.py
+++ iwtros_goal/scripts/iiwa_ptp_lin_motion.py
@@ -15,30 +15,30 @@ __REQUIRED_API_VERSION__ = "1"
 __ROBOT_VELOCITY__ = 0.1
 __ROBOT_VELOCITY_LIN__ = 0.07
 __ROBOT_ACCELERATION_LIN__ = 0.08
-_DEFAULT_PLANNING_GROUP = "panda_arm"
-PLANNING_GROUP_NAME = "panda_arm"
+_DEFAULT_PLANNING_GROUP = "iiwa_arm"
+PLANNING_GROUP_NAME = "iiwa_arm"
 
 def start_programme():
     rospy.loginfo("Start the program")
 
     start_pose = [0.45, -0.054, 0.308, -3.127, -0.014, -0.798]
-    pick_pose = Pose(position=Point(0.66, 0.25, 0.3), orientation=from_euler(math.radians(0), math.radians(180), math.radians(135)))
-    Place_pose = Pose(position=Point(0.50, -0.25, 0.3), orientation=from_euler(math.radians(0), math.radians(180), math.radians(135)))
+    pick_pose = Pose(position=Point(0.60, 0.25, 0.5), orientation=from_euler(math.radians(180), math.radians(0), math.radians(0)))
+    Place_pose = Pose(position=Point(0.6, -0.25, 0.5), orientation=from_euler(math.radians(180), math.radians(0), math.radians(0)))
     Other_pose = Pose(position=Point(0.66, 0, 0.3), orientation=from_euler(math.radians(0), math.radians(90), math.radians(0)))
 
     # move to start point joint values to avoid random trajectory
-    r.move(Ptp(goal=pick_pose, vel_scale=0.2, acc_scale=0.2, reference_frame="panda_link0", planning_group="panda_arm", target_link="panda_link8"))
+    r.move(Ptp(goal=pick_pose, vel_scale=0.2, acc_scale=0.2, reference_frame="iiwa_link_0", planning_group="iiwa_arm", target_link="iiwa_link_ee"))
     rospy.loginfo("Start pick and place loop....--->")
     while not rospy.is_shutdown():
         #Pick pose
         rospy.loginfo("move the robot to Place position")
-        r.move(Ptp(goal=Place_pose,  vel_scale=0.2, acc_scale=0.2, reference_frame="panda_link0", planning_group="panda_arm", target_link="panda_link8"))
+        r.move(Ptp(goal=Place_pose,  vel_scale=0.2, acc_scale=0.2, reference_frame="iiwa_link_0", planning_group="iiwa_arm", target_link="iiwa_link_ee"))
         Pick_and_Place()
-        #r.move(Ptp(goal=Other_pose,  vel_scale=0.2, acc_scale=0.2, reference_frame="panda_link0", planning_group="panda_arm", target_link="panda_link8"))
+        #r.move(Ptp(goal=Other_pose,  vel_scale=0.2, acc_scale=0.2, reference_frame="iiwa_link_0", planning_group="iiwa_arm", target_link="iiwa_link_ee"))
 
         #Place pose
         rospy.loginfo("Move to Pick position") # log
-        r.move(Ptp(goal=pick_pose,  vel_scale=0.2, acc_scale=0.2, reference_frame="panda_link0", planning_group="panda_arm", target_link="panda_link8"))
+        r.move(Ptp(goal=pick_pose,  vel_scale=0.2, acc_scale=0.2, reference_frame="iiwa_link_0", planning_group="iiwa_arm", target_link="iiwa_link_ee"))
         rospy.loginfo("Pick movement") # log
         Pick_and_Place()
         # try:
@@ -50,15 +50,15 @@ def start_programme():
 
 def Pick_and_Place():
     # the position is given relative to the TCP.
-    r.move(Lin(goal=Pose(position=Point(0, 0, 0.1)), vel_scale=0.05, acc_scale=0.05, reference_frame="panda_link8", planning_group="panda_arm", target_link="panda_link8"))
+    r.move(Lin(goal=Pose(position=Point(0, 0, 0.1)), vel_scale=0.03, acc_scale=0.03, reference_frame="iiwa_link_ee", planning_group="iiwa_arm", target_link="iiwa_link_ee"))
     rospy.loginfo("Open/Close the gripper") # log
     rospy.sleep(0.2)    # pick or Place the PNOZ (close or open the gripper)
-    r.move(Lin(goal=Pose(position=Point(0, 0, -0.1)), vel_scale=0.05, acc_scale=0.05, reference_frame="panda_link8", planning_group="panda_arm", target_link="panda_link8"))
+    r.move(Lin(goal=Pose(position=Point(0, 0, -0.1)), vel_scale=0.03, acc_scale=0.03, reference_frame="iiwa_link_ee", planning_group="iiwa_arm", target_link="iiwa_link_ee"))
 
 
 if __name__ == "__main__":
-    rospy.init_node("panda_pick_place_ptp_lin_node")
+    rospy.init_node("iiwa_pick_place_ptp_lin_node")
 
     r = Robot(__REQUIRED_API_VERSION__)
-    sequence = Sequence()
+    #sequence = Sequence()
     start_programme()
