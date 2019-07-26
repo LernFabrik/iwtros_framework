3b0463c42b1936102bac7c4b5ed38b7e7cd67010
VishnuPC <prasad.vishnu11@gmail.com>
2019-07-26 16:05:14 +0200

    Working(sim): IKFast Plugin for iiwa7

---
+++ iwtros_moveit_configs/iiwa7_ikfast_iiwa_arm_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py iiwa7 iiwa_arm iiwa7_ikfast_iiwa_arm_plugin /home/iwt-ros/ros_ws/kinematic_ws/src/iiwa7_ikfast_iiwa_arm_plugin/src/iiwa7_iiwa_arm_ikfast_solver.cpp
