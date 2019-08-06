80c4b325490b2c90844ca4aa2187e7f45869fb2a
VishnuPC <prasad.vishnu11@gmail.com>
2019-08-06 15:45:37 +0200

    Added collision object and visual markers
    collision object is working fine, but visual marker is not show at required position

--- iwtros_goal/src/iiwa_pick_and_place.cpp
+++ iwtros_goal/src/iiwa_pick_and_place.cpp
@@ -11,8 +11,12 @@
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
 
-#include <pilz_msgs/MotionSequenceRequest.h>
 
 static std::string PLANNING_GROUP = "iiwa_arm";
 
@@ -43,13 +47,46 @@ void robotSetupChecker(){
     }
 }
 
+/** Using planning scene interface the collision objects are attached
+ * @breif the size and poses are need to be redifined
+ */
+void addCollisionObject(moveit::planning_interface::PlanningSceneInterface& planning_scene_interface){
+    std::vector<moveit_msgs::CollisionObject> collision_obj;
+    collision_obj.resize(1);
+    collision_obj[0].id = "object";
+    collision_obj[0].header.frame_id = "iiwa_link_0";
+    collision_obj[0].primitives.resize(1);
+    collision_obj[0].primitives[0].type = shape_msgs::SolidPrimitive::CYLINDER;
+    collision_obj[0].primitives[0].dimensions.resize(2);
+    collision_obj[0].primitives[0].dimensions[0] = 0.02;
+    collision_obj[0].primitives[0].dimensions[1] = 0.02;
+    collision_obj[0].primitive_poses.resize(1);
+    collision_obj[0].primitive_poses[0].position.x = 0.0;
+    collision_obj[0].primitive_poses[0].position.y = -0.5;
+    collision_obj[0].primitive_poses[0].position.z = 0.01;
+    collision_obj[0].operation = collision_obj[0].ADD;
+
+    planning_scene_interface.applyCollisionObjects(collision_obj);
+
+}
 
 void ExecutePose(moveit::planning_interface::MoveGroupInterface& iiwa_group, geometry_msgs::PoseStamped target_pose){
     const robot_state::JointModelGroup* joint_model_group = 
                                 iiwa_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+    
+    /*Visual tool initialization */
+    namespace rvt = rviz_visual_tools;
+    moveit_visual_tools::MoveItVisualTools visual_tools("iiwa_link_ee");
+    visual_tools.deleteAllMarkers();
+    visual_tools.loadRemoteControl();
+    Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+    text_pose.translation().z() = 0.2;
+    visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+    visual_tools.trigger();
+    //visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
     moveit_msgs::OrientationConstraint oriCon;
-    oriCon.header.frame_id = "/iiwa_interface";
+    oriCon.header.frame_id = "iiwa_link_0";
     oriCon.link_name = "iiwa_link_ee";
     oriCon.orientation = target_pose.pose.orientation;
     oriCon.absolute_x_axis_tolerance = 0.01;
@@ -75,7 +112,7 @@ void ExecutePose(moveit::planning_interface::MoveGroupInterface& iiwa_group, geo
     motionReq.constraints.resize(1);
     motionReq.constraints[0].orientation_constraints.push_back(oriCon);
     motionReq.constraints[0].position_constraints.resize(1);
-    motionReq.constraints[0].position_constraints[0].header.frame_id = "/iiwa_interface";
+    motionReq.constraints[0].position_constraints[0].header.frame_id = "iiwa_link_0";
     motionReq.constraints[0].position_constraints[0].link_name = "iiwa_link_ee";
     motionReq.constraints[0].position_constraints[0].constraint_region.primitive_poses.resize(1);
     motionReq.constraints[0].position_constraints[0].constraint_region.primitive_poses[0] = target_pose.pose;
@@ -90,6 +127,14 @@ void ExecutePose(moveit::planning_interface::MoveGroupInterface& iiwa_group, geo
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
     moveit::planning_interface::MoveItErrorCode eCode = iiwa_group.plan(my_plan);
     ROS_INFO("Motion planning is: %s", eCode?"Success":"Failed");
+
+    /*Visualize the plan before the execution */
+    visual_tools.publishAxisLabeled(target_pose.pose, "Pose");
+    visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+    visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+    visual_tools.trigger();
+    //visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
     if(eCode){
         iiwa_group.execute(my_plan);
     }
@@ -110,6 +155,7 @@ int main(int argc, char** argv){
     // robotSetupChecker();
     
     moveit::planning_interface::MoveGroupInterface iiwa_group(PLANNING_GROUP);
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
     iiwa_group.setPlannerId("PTP");
     iiwa_group.setMaxVelocityScalingFactor(0.2);
     iiwa_group.setMaxAccelerationScalingFactor(0.2);
@@ -123,7 +169,7 @@ int main(int argc, char** argv){
 
     //iiwa_group.setNamedTarget("iiwa_Pose1");
     geometry_msgs::PoseStamped target_pose;
-    target_pose.header.frame_id = "/iiwa_interface";
+    target_pose.header.frame_id = "iiwa_link_0";
     target_pose.header.stamp = ros::Time::now()+ros::Duration(2.1);
     target_pose.pose.position.x = 0.4;
     target_pose.pose.position.y = 0;
@@ -131,12 +177,14 @@ int main(int argc, char** argv){
     target_pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI, 0, M_PI/2);
 
     geometry_msgs::PoseStamped target_pose2;
-    target_pose2.header.frame_id = "/iiwa_interface";
+    target_pose2.header.frame_id = "iiwa_link_0";
     target_pose2.header.stamp = ros::Time::now() + ros::Duration(2.1);
     target_pose2.pose.position.x = -0.4;
     target_pose2.pose.position.y = 0.2;
     target_pose2.pose.position.z = 1.4;
     target_pose2.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI, 0, 0);
+
+    addCollisionObject(planning_scene_interface);
 
     /* Refere the Readme*/
     while (ros::ok())
